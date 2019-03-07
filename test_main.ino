cff83329f7715097fa6f9c380457f9cfcc767fc0
ashima-24 <jindal241995@gmail.com>
2019-03-07 11:38:01 +0100

    Wifi connected touch sensor

---
+++ test_main.ino
@@ -0,0 +1,76 @@
+#include <CapacitiveSensor.h>
+#include "ESP8266WiFi.h"
+  #include <ESP8266HTTPClient.h>
+// touch sensor config
+CapacitiveSensor capSensor1 = CapacitiveSensor(D4, D2);
+
+
+// WiFi parameters
+const char* ssid = "pi";
+const char* password = "unisaarland";
+
+const char* host = "192.168.137.184";
+long sensorValue1;
+WiFiClient client;
+
+void setup() 
+{
+  Serial.begin(115200);
+ 
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+   Serial.println(WiFi.localIP());
+
+ 
+}
+
+void loop()
+ {
+  
+    long start = millis();
+     sensorValue1 = capSensor1.capacitiveSensor(30);
+    
+    Serial.print("\t");
+    
+    Serial.println(sensorValue1);
+  
+  delay(500);
+  
+ if(WiFi.status()== WL_CONNECTED)
+ {   //Check WiFi connection status
+ 
+ if(client.connect("192.168.137.184", 3000))
+ {
+//  client.println("host : 192.168.137.184");
+Serial.println("host : 192.168.137.184");
+Serial.println("connected");
+  //client.println("hello from ESP8266");
+  }
+ 
+ 
+ String url = "/sensor?command=sensor&value=";
+    url += (sensorValue1);
+    url += "&ip=";
+    url += WiFi.localIP().toString();
+
+    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+
+
+  Serial.println("wait 1 sec...");
+  delay(1000);
+ 
+ 
+  
+
+
+ }
+  
+}
