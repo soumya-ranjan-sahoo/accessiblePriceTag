8c85cc01358afecd903b3157c98e91d44d9f59b8
ashima-24 <44822013+ashima-24@users.noreply.github.com>
2019-04-16 18:39:02 +0200

    Update price.ino

--- price.ino
+++ price.ino
@@ -1,9 +1,9 @@
 #include <CapacitiveSensor.h>
 #include "ESP8266WiFi.h"
-  #include <ESP8266HTTPClient.h>
+ #include <ESP8266HTTPClient.h>
   
 const int numReadings = 4;
-const int val = 1000;
+const int val = 1000; // Number of values read by sensor
 int readings[val];      // the readings from the  input
 int readIndex = 0;              // the index of the current reading
 int total = 0;                  // the running total
@@ -15,23 +15,20 @@ int count = 0 ;
 // touch sensor config
 CapacitiveSensor capSensor1 = CapacitiveSensor(D4, D2);
 
-
-
 // WiFi parameters
 const char* ssid = "pi";
 const char* password = "unisaarland";
 
-const char* host = "192.168.5.1";
-long sensorValue1;
+const char* host = "192.168.5.1"; // Pi server IP
+long sensorValue1; // Variable for reading sensor values
 WiFiClient client;
+
 void setup() 
 {
-
   Serial.begin(115200);
-  
- 
-  // Connect to WiFi
-WiFi.begin(ssid, password);
+   
+ // Connect to WiFi
+  WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) 
   {
     delay(500);
@@ -41,17 +38,16 @@ Serial.println("");
 Serial.println("WiFi connected");
 Serial.println(WiFi.localIP());
 
+// initialising the reading variable 
 for (int thisReading = 0; thisReading < val; thisReading++)
 {
     readings[thisReading] = 0;
-    
 }
 }
 
 void loop()
  {
-  
-  // read from the sensor:
+  // read values  from the sensor:
   long start = millis();
   long sensorValue1 = capSensor1.capacitiveSensor(30);
   readings[readIndex] = sensorValue1;
@@ -60,28 +56,31 @@ void loop()
   total = total + readings[readIndex];
   // advance to the next position in the array:
   readIndex = readIndex + 1;
+  count = count +1; // counter to check the number of values read by the sensor
 
- 
-count = count +1;
-
+  // Here we are checking if we got 4 sensor values and wifi connected 
 
  if(WiFi.status()== WL_CONNECTED && count ==4)
   {   //Check WiFi connection status
    
        Serial.println("host : 192.168.5.1");
        Serial.println("connected");
-
-       average = total / numReadings;
-        Serial.println(average);
    
+        // taking average of 4 sensor values
+       average = total / numReadings;
+       Serial.println(average);
+   
+        // Here we are only sending the average of 4 sensor values   
         String url = "/sensor?command=sensor&value=";
         url += average;
-        url += "&id=";
+        url += "&id=";    // Variable for detecting from which wemos, touch input is coming
         url += "wemos1";
-
+   
+        // Here we are checking if client is  also connected
        if(client.connect("192.168.5.1", 3000))
          {
             Serial.println("client");
+           // Sending only those touch values whose average is greater than 15 
                 if(average > 15)
                 {
                     client.print(String("GET ") + url + " HTTP/1.1\r\n" +
@@ -89,12 +88,12 @@ count = count +1;
                     "Connection: close\r\n\r\n");
                 }
          }
-       
+  // reinitialising count, total, average     
   count = 0;
   total = 0;
   average = 0;
  }
  Serial.print("\t");  
  Serial.println(sensorValue1);
- delay(250);  
+  delay(250);  
  }
