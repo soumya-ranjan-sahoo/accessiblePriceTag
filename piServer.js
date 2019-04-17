70a6054e0bf29611bcea4d4e7cdfadf4596b45ef
ashima-24 <44822013+ashima-24@users.noreply.github.com>
2019-04-17 20:18:32 +0200

    Update piServer.js

--- piServer.js
+++ piServer.js
@@ -15,8 +15,8 @@ app.get('/', (req, res) => {
 res.send('Pi Server is running on port 3000')
 })
 // wemos: variable for getting which wemos is sending touch input first or second
-// t : variable for getting touch sensor value from wemos
-//pt : variable used to get the socket id of phone which has made the socket connection
+// t : variable for obtaining touch sensor value from wemos
+//pt : variable used to get the socket id of device(smart phone) which has made the socket connection
 var t,wemos, pt;
 
 server.listen(3000,()=>{
@@ -26,7 +26,7 @@ console.log('Accessible Price Tag Server is running on port 3000')
 })
 // Socket Connection started
 io.on('connection', (socket) => {
- // Socket Connection is made with phone
+ // Socket Connection is made with mobile device
 console.log('made socket connection',socket.id)
 
 
@@ -34,7 +34,7 @@ socket.on('beacon', (messageContent) => {
 console.log(messageContent)
 
 
-// Getting the touch sensor values from wemos 
+// Obtaining the touch sensor values from wemos 
 app.use('/sensor',function(req,res,next)
 {
  t = req.param('value');
@@ -43,7 +43,7 @@ wemos = req.param('id');
 console.log("id = " + wemos);
 
 // For first handshake =>
- //Server sending a scan value to all  phones which are  near to beacon
+//Sending a scan event to all mobile devices which are near to product
 let message = {"scan":"touched"}
 socket.broadcast.emit('scan',message)
 console.log("1st handshake completed : Server -----> Phone ");
@@ -52,7 +52,7 @@ console.log("1st handshake completed : Server -----> Phone ");
 })
 })
 // For second handshake =>
- // Phone will be sending beacon-id back to server (if it is near to the beacon  )
+ // This listens to beacon-id sent from mobile device
 socket.on('beacon-id', (messageContent1) => {
 console.log("2nd handshake completed : Phone -----> Server");
 console.log(messageContent1) // messageContent1 : beacon-id
@@ -61,7 +61,9 @@ console.log("socket id = ",pt);
 
 
 // After completion of two handshakes, now price is sent in third handshake
-//for one beacon system = > if the wemos1 is sending touch input and near to Jon beacon(beetroot)
+//for one beacon system = > if the wemos1 is sending touch input and near to  beacon named set as Jon 
+ // which can be configured on estimote cloud
+ 
  // Then only emit the price of the product 
 if( wemos.trim() == "wemos1".trim() && messageContent1.trim() === "Jon".trim())
 {
@@ -71,9 +73,9 @@ io.to(socket.id).emit('price',price_tag1);
 
 }
 
-
-//for one beacon system = > if the wemos2 is sending touch input and near to Me beacon(candy floss)
- // Then only emit the price of the product
+ //for one beacon system = > if the wemos2 is sending touch input and near to  beacon named set as Me
+ //which can be configured on estimote cloud
+ // Then it emits the price of the product
  if( wemos.trim() == "wemos2".trim() && messageContent1.trim() === "Me".trim())
 {
 
